338439a3f3d34e84c76c7664e426e496a887eb75
kelvin beno <kelvinbeno526@gmail.com>
2022-03-28 19:01:12 +0300

    loader update

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Grid from '@mui/material/Grid';
 import beno from '../../assets/beno.jpg'
 import './Loading.css'
-import CircularProgress from '@mui/material/CircularProgress';
+import LinearProgress from '@mui/material/LinearProgress';
 
 
 
@@ -11,9 +11,10 @@ export default function Loading() {
   return (
     <div className="load">
             <Grid item xs ={12} xl={12} l={12} sm={12} className="img-loading">
-           <img style={{width: '277px'}} src={beno} alt="" srcset="" className="gif" />
-           <Grid style={{display: 'flex', justifyContent: 'center', position: 'absolute', top: 434, left: 188}}>
-           <CircularProgress  color="secondary"/>
+           <img style={{width: '277px'}} src={beno} alt="" srcset="" className="benoImg" />
+           <Grid>
+           <LinearProgress color="secondary" />
+
            </Grid>
             </Grid>
         </div>
