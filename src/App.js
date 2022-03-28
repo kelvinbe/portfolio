e871dac9cbd29a5acc1578ec09de152cd204d12a
kelvin beno <kelvinbeno526@gmail.com>
2022-03-28 19:08:05 +0300

    removed loader

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
 import './App.css';
 import Home from './components/Home/Home';
-import React, { useState, useEffect } from 'react'
-import Loading from './components/Loading/Loading';
+import React from 'react'
+// import Loading from './components/Loading/Loading';
 
 function App() {
 
-  const [loading, setLoading] = useState(true)
+  // const [loading, setLoading] = useState(true)
 
-  useEffect(() => {
-    setTimeout(() => setLoading(false), 5000)
-  }, [])
+  // useEffect(() => {
+  //   setTimeout(() => setLoading(false), 5000)
+  // }, [])
 
   return (
-    <>
-    { loading === false ?(
+  
     <div className='App'>
     <Home />
     </div>
-    ):
-    <div>
-      <Loading />
-    </div>
-    }
-    </>
+   
   );
 }
 
