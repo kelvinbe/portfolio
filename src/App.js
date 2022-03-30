31b3eafc4bbf55e7a4f12a5989f18e704c0bfb97
kelvin beno <kelvinbeno526@gmail.com>
2022-03-30 17:56:04 +0300

    created a header

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import './App.css';
 import Home from './components/Home/Home';
-import React from 'react'
-// import Loading from './components/Loading/Loading';
+import React, { useState, useEffect } from 'react'
+import Loading from './components/Loading/Loading';
 
 function App() {
 
-  // const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true)
+  const [alreayLoaded, setAlreadyLoaded] = useState(false)
 
-  // useEffect(() => {
-  //   setTimeout(() => setLoading(false), 5000)
-  // }, [])
+  useEffect(() => {
+    setTimeout(() => (setLoading(false), 5000), () => setAlreadyLoaded(true), 1000)
+  }, [])
 
   return (
-  
+    <>
+    {
+    loading === false && alreayLoaded === true ? (
     <div className='App'>
     <Home />
-    </div>
-   
+    </div>): 
+    <Loading />
+}
+    </>
   );
 }
 
